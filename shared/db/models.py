42dfbba9e37247d45b11e32a0679cd6ccca65897
woo <adresshfx@gmail.com>
2026-03-11 10:22:37 +0500

    feat(shared): add user table

--- shared/db/models.py
+++ shared/db/models.py
@@ -1,1 +1,21 @@
+from sqlalchemy import BigInteger
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship
+)
+
 from shared.db.base import Base
+
+
+class User(Base):
+    '''таблица юзера'''
+
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    role: Mapped[str] = mapped_column(default="user")
+    first_name: Mapped[str]
+    last_name: Mapped[str | None]
+    username: Mapped[str | None]
+    phone: Mapped[str | None]
