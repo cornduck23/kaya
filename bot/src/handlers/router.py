14576052bf73e027644a87691ad2923174386adb
woo <adresshfx@gmail.com>
2026-03-15 18:47:52 +0500

    feat(bot): profile handler

--- bot/src/handlers/router.py
+++ bot/src/handlers/router.py
@@ -1,11 +1,23 @@
 from aiogram import Router
+from aiogram.enums import ParseMode
 from aiogram.types import Message
 from aiogram.filters.command import Command
+
+from shared.services.user import UserService
+from src.utils import get_profile
 
 
 root = Router()
 
 
 @root.message(Command("start"))
-async def start(message: Message, state) -> None:
+async def start(message: Message, session) -> None:
+    await UserService(session).get_or_create(message.from_user)
     await message.answer(text="start")
+
+
+@root.message(Command("profile"))
+async def show_profile(message: Message, session) -> None:
+    user = await UserService(session).get_user(message.from_user.id)
+    profile = get_profile(user)
+    await message.answer(text=profile, parse_mode=ParseMode.MARKDOWN_V2)
---
+++ bot/src/utils.py
@@ -0,0 +1,7 @@
+def get_profile(user):
+    result = (
+        f"🫆\n"
+        f"id: `{user.id}`\n"
+        f"first name: `{user.first_name}`"
+    )
+    return result
